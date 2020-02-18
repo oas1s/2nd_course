c5b53b7c283efea46234bc401a4116ca645fbc6f
oas1s <azatazat835@mail.ru>
2020-02-18 11:09:37 +0300

    hw1 w/ pag w/out search added

--- hw1/src/main/java/ru/itis/notes/controllers/NoteController.java
+++ hw1/src/main/java/ru/itis/notes/controllers/NoteController.java
@@ -4,6 +4,7 @@ package ru.itis.notes.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,9 +14,28 @@ import java.util.List;
 @Controller
 public class NoteController {
     List notes = new ArrayList();
-    @GetMapping("/notes")
-    public String getNotes(Model model){
-        model.addAttribute("notes", notes);
+    List pagNotes = new ArrayList();
+    int pagPages = 0;
+    @GetMapping("/notes/{var}")
+    public String getNotes(Model model, @PathVariable(required = false) int var){
+        if (var == 0) {
+            return "redirect:notes/1";
+        }
+        pagNotes.clear();
+        int end = var*5;
+        int start = end-5;
+
+        if (end>notes.size()) {
+            end = notes.size();
+        }
+
+        for (int i = start; i <end; i++) {
+            if (notes.get(i) != null) {
+                pagNotes.add(notes.get(i));
+            }
+        }
+
+        model.addAttribute("notes", pagNotes);
     return "notes";
     }
 
